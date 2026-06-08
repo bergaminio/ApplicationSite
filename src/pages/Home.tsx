6d3bab228ca87418f89a076449af3bf83bbb16f6
Michael Bergamin <michael.bergamin@proton.me>
2026-06-08 18:07:50 +0200

    feat: add tailwind css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
 function Home() {
   return (
-    <div>
-      <h1>Michael Bergamin</h1>
-      <p>Developer Student at BWD Bern</p>
+    <div className="p-8">
+      <h1 className="text-4xl font-bold text-blue-500">Michael Bergamin</h1>
+      <p className="text-gray-500 mt-2">Developer Student at BWD Bern</p>
     </div>
   )
 }
